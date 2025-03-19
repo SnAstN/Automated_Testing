f624c977bef3a675e42007c8c6cc9d402a4de023
Snezhanna <snezhannaast@yandex.ru>
2025-03-19 23:30:02 +0300

    warning: in the working copy of 'Homework/2/Project1/.gitignore', LF will be replaced by CRLF the next time Git touches it

---
+++ Homework/2/Project1/src/test/java/RegistrationFormTest.java
@@ -0,0 +1,39 @@
+package src.test.org.example;
+import org.example.RegistrationForm;
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class RegistrationFormTest {
+    @Test
+    public void CheckTestAllRight(){ // все данные корректны
+        RegistrationForm registrationForm = new RegistrationForm();
+        boolean result = registrationForm.Check("User", "user12@gmail.com", "124376asdf1");
+        Assertions.assertTrue(result);
+    }
+
+
+    @Test
+    public void CheckTestNameMistake(){ // в поле имя пустая строка
+        RegistrationForm registrationForm = new RegistrationForm();
+        boolean result = registrationForm.Check("", "Anna@gmail.ru", "fht!jyr67/9d");
+        Assertions.assertTrue(!result);
+    }
+
+    @Test
+    public void CheckTestEmailMistake(){ // пропущен символ @ в email
+        RegistrationForm registrationForm = new RegistrationForm();
+        boolean result = registrationForm.Check("Anna", "AnnAlVlyandex.com", "AnPassword12");
+        Assertions.assertTrue(!result);
+    }
+
+
+    @Test
+    public void CheckTestPasswordMistake(){ // длина пароля меньше 10
+        RegistrationForm registrationForm = new RegistrationForm();
+        boolean result = registrationForm.Check("Ivan", "IvanUser@gmail.ru", "12");
+        Assertions.assertTrue(!result);
+    }
+}
