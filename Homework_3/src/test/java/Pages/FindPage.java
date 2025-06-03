52f17c585f6916f9ba48b4e2eee0dee732585fd8
SnAstN <snezhannaast@yandex.ru>
2025-06-03 18:25:05 +0300

    Удалена папка Homework_3

--- Homework_3/src/test/java/Pages/FindPage.java
+++
@@ -1,5 +0,0 @@
-package Pages;
-
-public class FindPage implements BasePage {
-    public final String goBackButton = "//img[@class='toolbar_custom-logo_img __light']";
-}
--- Homework_3/src/test/java/Pages/GroupOnePage.java
+++
@@ -1,5 +0,0 @@
-package Pages;
-
-public class GroupOnePage {
-    public final String onegroup = "// h1[@class='group-name_h']";
-}
